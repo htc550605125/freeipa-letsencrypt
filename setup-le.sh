4faaec8073c073cd2f2615b11739412d51be4711
NullMDR <htc550605125@gmail.com>
2016-10-19 02:04:25 -0500

    Auto generated by Octophus

--- setup-le.sh
+++ setup-le.sh
@@ -5,10 +5,14 @@ WORKDIR="/root/ipa-le"
 
 dnf install letsencrypt -y
 
-ipa-cacert-manage install "$WORKDIR/ca/isrgrootx1.pem" -n ISRGRootX1 -t ,,
+ipa-cacert-manage install "$WORKDIR/ca/isrgrootx1.pem" -n ISRGRootX1 -t C,,
+ipa-cacert-manage install "$WORKDIR/ca/DSTRootCAX3.pem" -n DSTRootCAX3 -t C,,
 ipa-certupdate -v
 
-ipa-cacert-manage install "$WORKDIR/ca/LetsEncryptAuthorityX3.pem" -n letsencryptx3 -t C,,
+ipa-cacert-manage install "$WORKDIR/ca/LetsEncryptAuthorityX1.pem" -n letsencryptx1 -t ,,
+ipa-cacert-manage install "$WORKDIR/ca/LetsEncryptAuthorityX2.pem" -n letsencryptx2 -t ,,
+ipa-cacert-manage install "$WORKDIR/ca/LetsEncryptAuthorityX3.pem" -n letsencryptx3 -t ,,
+ipa-cacert-manage install "$WORKDIR/ca/LetsEncryptAuthorityX4.pem" -n letsencryptx4 -t ,,
 ipa-certupdate -v
 
 "$(dirname "$0")/renew-le.sh" "--first-time"
